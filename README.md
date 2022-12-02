2325ae4e3e350734e41c856302b7fac474a260f8
LappiBHEET <bheet@krk.finance>
2022-12-02 15:03:28 +0100

    dependencies dotenv

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Today we are going to talk about the things that we are facing in this history.
 ## Tasks
 
 - Parsing article data on the paths
+- Put titles in a list and then itterate them into a li
 - Use GithubAPI ✅
 - Github pages compatible 
 - Fix the sorting of the titles
--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-   var content = document.querySelector("main");
-   import key from "./token.json" assert {type: 'json'};
-    //Parse data throught .md parser
-    fetch(`./Posts/sample.md`)
-      .then((response) => response.text())
-      .then((md) => {
-        var mark = marked.parse(md);
-        //console.log(mark);
-      });
+require('dotenv').config();
+require('marked');
 
-      fetch(`https://api.github.com/repos/KRKBHEET/Blog/contents/Posts/`,{
+var content = document.querySelector("main");
+let li;
+let a;
+//Parse data throught .md parser
+fetch(`./Posts/sample.md`)
+  .then((response) => response.text())
+  .then((md) => {
+    var mark = marked.parse(md);
+    //console.log(mark);
+  });
+
+fetch(`https://api.github.com/repos/KRKBHEET/Blog/contents/Posts/`, {
+  'headers': {
+    'Authorization': process.env.GITHUB_ACCES_TOKEN
+  }
+})
+  .then((response) => response.json())
+  .then((GitPosts) => {
+    //Itterating the json
+    GitPosts.forEach(Post => {
+      //Removing the .md from titles
+      Post.name = Post.name.slice(0, (Post.name.length - 3))
+
+      //Creating the elements 
+      li = document.createElement("li");
+      a = document.createElement("a");
+
+      //Appending the titles
+      li.append(a);
+      a.innerText = Post.name;
+      a.href = Post.path;
+      content.append(li);
+      fetch(`https://api.github.com/repos/KRKBHEET/Blog/contents/Posts/${Post.name}.md?ref=master`, {
         'headers': {
-          'Authorization': key.GITHUB_ACCES_TOKEN 
+          'Authorization': process.env.GITHUB_ACCES_TOKEN
         }
-        })
-      .then((response) => response.json())
-      .then((GitPosts) => {
-        GitPosts.forEach(Post => {
-          Post.name = Post.name.slice(0,(Post.name.length - 3))
-          let li = document.createElement("li");
-          let a = document.createElement("a");
-          li.append(a);
-          a.innerText = Post.name;
-          a.href = Post.path;
-          content.append(li);
+      })
+        .then((response) => response.json())
+        .then((md) => {
+
+          //Decode base64
+          var decoded = atob(md.content);
+          console.log(Post.name)
+          console.log("__________________");
+          console.log(decoded);
         });
-        console.log("Title lists");
-        console.log(GitPosts);
-      });
-      let Gcontent= [];
-      fetch(`https://api.github.com/repos/KRKBHEET/Blog/contents/Posts/sample.md?ref=master`,{
-        'headers': {
-          'Authorization': key.GITHUB_ACCES_TOKEN 
-        }
-        })
-      .then((response) => response.json())
-      .then((md) => {
-        console.log("__________________");
-        var decoded = atob(md.content);
-        console.log(decoded);
-      });
+    });
+    console.log("Title lists");
+    console.log(GitPosts);
+    a.href = "template.html"
+    console.log(li)
+    return GitPosts;
+  });
